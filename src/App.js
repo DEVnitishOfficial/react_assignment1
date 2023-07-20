795ecb4f659c8f2842fa0467322f395de671bd32
DEVnitishOfficial <nitish.naroun123@gmail.com>
2023-07-20 11:08:36 +0530

    added assignmet.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Person, {Button,Header,List} from './assignment';
+
 
 function App() {
+  const handleButtonClick = () => {
+    return(  
+      <>
+       Congratulation🎉 <br/> Button clicked successfully!
+       </>
+       )
+  };
+  const listItems = ["Item 1", "Item 2", "Item 3", "Item 4"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+                 {/* assignment question one */}
+                <h4 className='centre'>
+                 Assignment question<span>one</span> solution
+                </h4>
+      <Person  name="Nitish" age="20"  />
+
+                 {/* assignment question two */}
+                 <h4 className='centre'>
+                 Assignment question <span>two</span> solution
+                </h4>
+
+      <Button   text="click me!" onClick={handleButtonClick} />
+
+                  {/* assignment question three */}
+                  <h4 className='centre'>
+                 Assignment question <span>three</span> solution
+                </h4>
+      <Header title="This is the header of my website"/>
+
+
+                   {/* assignment question four */}
+                  <h4 className='centre'>
+                  Assignment question <span>four</span> solution
+                   </h4>
+      <List items={listItems} />
+   </>
   );
 }
 
---
+++ src/assignment.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './assignment.css'
+import 'animate.css'
+
+/*
+Q1.  Create a component named "Person" that takes two props - "name" and "age". The component should  display the persons name and age in a paragraph element
+*/
+function Person(props){
+
+    return(
+        <>      
+      <p className="person-name">
+
+       {props.name} is {props.age} years old
+
+      </p>
+      </>
+    )
+}
+export default Person
+
+/*
+ Q2. Create a component named "Button" that takes two props - "text" and "onClick". The component should display a button element with the given text and call the onClick function when clicked
+*/
+
+ export function Button({text,onClick}){
+    const [output, setOutput] = useState('Button yet not clicked');
+
+    const handleClick = () => {
+      const result = onClick();
+      setOutput(result);
+    };
+    return(
+        <>
+      <button className='btn' onClick={handleClick}> {text} </button>
+
+      <p className='click-btn'>{output}</p>
+
+        </>
+    )
+}
+
+/*
+ Q3. Create a component named "Header" that takes one prop - "title". The component should display a header element with the given title.
+ */
+
+ export function Header(props){
+    return(
+       <h1 className='centre'>  {props.title}   </h1>
+    )
+ }
+
+ /**
+  * Q4.  Create a component named "List" that takes one prop - "items". The component should display an unordered list element with the given list items.
+  * 
+  */
+
+ export function List({items}){
+  return(
+    <>
+     <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+    </>
+  )
+ }
+
+
